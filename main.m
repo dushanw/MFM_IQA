95252b1404287c169a892489f53500e5264be3d8
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2020-10-20 16:45:45 +0530

    code on real data; added 3D registration for PS with NN and TF. Added ssim code on the volumes.

--- main.m
+++ main.m
@@ -65,7 +65,55 @@ disp(sprintf('SSIM: rot=%d, aRot=%d, PSNR:rot=%d, aRot=%d,',...
               ssimval_rot,ssimval_aRot,peaksnr_rot,peaksnr_aRot));
 
             
+%% SSIM on 3d registered real data
+of    = cd('./_Datasets/FullStacks/');                              % read real datasets
+Data  = f_readDataset();
+cd(of);
+% volshow(Data.PS,'Renderer','MaximumIntensityProjection');         % visualize datasets
+% subplot(1,3,1);imagesc(max(Data.PS,[],3));axis image
+% subplot(1,3,2);imagesc(max(Data.TF,[],3));axis image
+% subplot(1,3,3);imagesc(max(Data.NN,[],3));axis image
 
+[optimizer,metric]          = imregconfig('multimodal');            % 3d registration
+optimizer.MaximumIterations = 500;
+optimizer.InitialRadius     = 1e-4;
 
+delta       = 1;                                                    % for cropping later
+DataReg.NN  = imregister(Data.NN+delta,Data.PS,'similarity',optimizer,metric,'DisplayOptimization',1);
+DataReg.TF  = imregister(Data.TF+delta,Data.PS,'similarity',optimizer,metric,'DisplayOptimization',1);
 
+figure;                                                             % visualize NN registartion for the figure
+tlo = tiledlayout(2,4,'TileSpacing','none','Padding','none');
+nexttile, imagesc   (max(Data.PS,[],3));axis image;axis off;                 title('PS')
+nexttile, imagesc   (max(Data.NN,[],3));axis image;axis off;                 title('NN')
+nexttile, imshowpair(max(Data.NN,[],3),max(Data.PS,[],3));                   title('overlay')
+nexttile, imshowpair(histeq(max(Data.NN,[],3)),histeq(max(Data.PS,[],3)));   title('overlay(eqHist)')
+nexttile, imagesc   (max(Data.PS,[],3));axis image;axis off;           title('PS')
+nexttile, imagesc   (max(DataReg.NN,[],3));axis image;axis off;              title('NN registered')
+nexttile, imshowpair(max(DataReg.NN,[],3),max(Data.PS,[],3));                title('overlay')
+nexttile, imshowpair(histeq(max(DataReg.NN,[],3)),histeq(max(Data.PS,[],3)));title('overlay(eqHist)')
+set(tlo.Children,'XTick',[], 'YTick', [],'fontsize',16);
+
+figure;                                                             % visualize TF registartion for the figure 
+tlo = tiledlayout(2,4,'TileSpacing','none','Padding','none');
+nexttile, imagesc   (max(Data.PS,[],3));axis image;axis off;                 title('PS')
+nexttile, imagesc   (max(Data.TF,[],3));axis image;axis off;                 title('TF')
+nexttile, imshowpair(max(Data.TF,[],3),max(Data.PS,[],3));                   title('overlay')
+nexttile, imshowpair(histeq(max(Data.TF,[],3)),histeq(max(Data.PS,[],3)));   title('overlay(eqHist)')
+nexttile, imagesc   (max(Data.PS,[],3));axis image;axis off;                 title('PS')
+nexttile, imagesc   (max(DataReg.TF,[],3));axis image;axis off;              title('TF registered')
+nexttile, imshowpair(max(DataReg.TF,[],3),max(Data.PS,[],3));                title('overlay')
+nexttile, imshowpair(histeq(max(DataReg.TF,[],3)),histeq(max(Data.PS,[],3)));title('overlay(eqHist)')
+set(tlo.Children,'XTick',[], 'YTick', [],'fontsize',16);
+
+ssim_sigma = 1;                                                     % run SSIM on the registered (and original) data   
+[ssimval_nn ,ssimmap_nn ] = ssim(Data.PS,Data.NN   ,'Radius',ssim_sigma);
+[ssimval_rnn,ssimmap_rnn] = ssim(Data.PS,DataReg.NN,'Radius',ssim_sigma);
+
+[ssimval_tf ,ssimmap_tf ] = ssim(Data.PS,Data.TF   ,'Radius',ssim_sigma);
+[ssimval_rtf,ssimmap_rtf] = ssim(Data.PS,DataReg.TF,'Radius',ssim_sigma);
+
+figure;                                                             % plot ssim maps  
+subplot(1,2,1);imagesc(min(ssimmap_rnn,[],3));axis image;axis off;colorbar;  title('SSIM NN'); set(gca,'fontsize',16);
+subplot(1,2,2);imagesc(min(ssimmap_rtf,[],3));axis image;axis off;colorbar;  title('SSIM TF'); set(gca,'fontsize',16);
 
